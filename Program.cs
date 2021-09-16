31cd65d868bf9613ba2b127b16ce9d9a2661cddd
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-09-16 20:27:21 -0300

    Inserindo a variavel double e transcrevendo a formula para o programa

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular a área do triangulo retangulo");
+
+            Console.WriteLine();
+
+            Console.Write("Digite a base do triangulo: ");
+            string baseValor = Console.ReadLine();
+            double baseNumero = Int32.Parse(baseValor);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do triangulo: ");
+            string alturaValor = Console.ReadLine();
+            double alturaNumero = Int32.Parse(alturaValor);
+
+            double areaTriagulo = (baseNumero*alturaNumero)/2;
+
+            Console.WriteLine();
+
+            Console.WriteLine("A área do triangulo retangulo é: " + areaTriagulo);
+        }
+    }
+}
